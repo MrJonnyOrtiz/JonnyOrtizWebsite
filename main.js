4efef1bcdd1dcf2209dac7cfc655bb5eedeebbb0
Jonny Ortiz <j0nny.0rt1z@yahoo.com>
2023-09-14 15:27:54 -0400

    updated 2nd statement

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 
    const statements = [
       "Hi, I'm Jonny!",
-      "I'm a freelance Full Stack Web Developer specializing in AWS serverless apps.",
+      "I'm a freelance Full Stack Web Developer specializing in React and AWS serverless apps.",
       'Email me at <a href="mailto:mr.jonny.ortiz@gmail.com">mr.jonny.ortiz@gmail.com</a>.',
       'Check out my code on <a href="https://github.com/MrJonnyOrtiz" target="_blank" rel="noreferrer" rel="noopener">Github</a>',
       'Check out my blog posts:<br><a href="https://medium.com/@imj0nny0rt1z/react-rails-project-planning-process-711839a94b20" target="_blank" rel="noreferrer" rel="noopener">React & Rails Project Planning Process</a><br><a href="https://medium.com/@imj0nny0rt1z/general-javascript-coding-best-practices-800391d91c18" target="_blank" rel="noreferrer" rel="noopener">General Javascript Coding Best Practices</a><br><a href="https://medium.com/@imj0nny0rt1z/react-ed-8b2248c8b807" target="_blank" rel="noreferrer" rel="noopener">React-ed</a><br><a href="https://www.sarasotaremodeling.com/blog.html" target="_blank" rel="noreferrer" rel="noopener">More articles for a client</a>',
